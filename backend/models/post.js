b53275822584e79cf50c57a376d0acb001a3ce47
DRaymond <danimator1@gmail.com>
2024-06-21 14:20:53 -0400

    added post model

---
+++ backend/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Post = new Schema(
+    {
+        Content: {type: String, required: true},
+        Attacthments: {type: String, required: false},
+        Author: { type: Schema.Types.ObjectId, ref: 'UserID'},
+        Likes: {type: Number, required: true},
+        Comments: {type: String, required: true}
+    }, 
+    {timestamps: true}
+)
+
+module.exports = mongoose.model('posts', Post)
