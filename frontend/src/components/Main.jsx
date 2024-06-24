04c79960e0430de5aa1cbe11b32ad79a9ccca851
graceCancod3 <gracem@graces-iMac.local>
2024-06-24 10:16:48 -0400

    header route added

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import Nav from './Nav'
 import Home from './Home'
 import TBD from './TBD'
+import Header from './Header'
 
 const Main = (props) => {
 
     return (
         <div className="main">
+            <Header />
             <Nav/>
             <Home/>
         </div>
