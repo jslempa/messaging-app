33cb08b54da577c107d375a88369e85bfc4ea671
jslempa <jslempa@gmail.com>
2024-06-24 10:06:19 -0400

    finished comment controller, started Posts.jsx

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,14 +1,39 @@
+import { useState, useEffect } from 'react'
 import Likes from './Likes'
 import Comments from './Comments'
 
 const Posts = (props) => {
 
-    return (
-        <div className="posts">
-            <Likes/>
-            <Comments/>
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const res = await axios.get(`${BASE_URL}`)
+            setPosts(res)
+        }
+        getPosts()
+    }, [])
+    
+    return posts ? (
+        <div className='posts'>
+            {
+                posts.map((post) => { 
+                    <div className='post-single'>
+                        
+                    </div>
+                
+                })
+           
+           
+           
+       
+            }    
+          
+
         </div>
-    )
+    ) : null
 }
 
 export default Posts
+
+// http://127.0.0.1:3001
