c196ee690ba35ac1d857b3f5f3c693a074c8f163
graceCancod3 <gracem@graces-iMac.local>
2024-06-24 10:08:28 -0400

    set up header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,25 @@
 const Header = () => {
 
     return (
-  <></>
+        <div className="profile">
+        <div className="image">
+            {/* axios call for user's picture <img>picture</img> */}
+        </div>
+        <div className="contact-info">
+             {/* axios call for user's email */}
+        </div>
+        <div className="bio">
+            {/* axios call for user's bio <h4>bio</h4>*/}
+        </div>
+        <div className="following">
+            {/* axios call for followers
+             <h3>
+                followers:
+                following:
+            </h3>   */}
+        </div>
+
+        </div>
 
 
 
