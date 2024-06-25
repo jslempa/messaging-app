4a394a393395984f96ff51e57315a2bcbf3dc1f9
Tobias Dodge <tobydodge65@gmail.com>
2024-06-25 15:10:38 -0230

    I think the modal is working sort of, prints value to the console

--- frontend/src/components/PostModal.jsx
+++ frontend/src/components/PostModal.jsx
@@ -1,14 +1,45 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './PostModal.css'
 
 const PostModal = ({isOpen, onClose}) => {
     if (!isOpen) return null
 
+    const [postContent, setPostContent] = useState('')
+    const [userID, setUserID] = useState('')
+
+    const handlePost = async (e) => {
+        e.preventDefault()
+
+        //const response = await axios.post('http://localhost:5000/posts', { postContent, userID })
+        console.log(postContent)
+        console.log(userID)
+        onClose()
+    }
+
+    const handleChange = (e) => {
+        setPostContent(e.target.value)
+    }
+
+    const handleUser = (e) => {
+        setUserID(e.target.value)
+    }
+
     return (
         <div className='post-modal'>
             <div className='card'>
-                <form>
-                    <textarea placeholder='Content' />
+                <form onSubmit={handlePost}>
+                    <input type="text" 
+                    placeholder='User ID'
+                    value={userID}
+                    onChange={handleUser}
+                    />
+
+                    <textarea placeholder='Content' 
+                    value={postContent}
+                    onChange={handleChange}
+                    />
+
                     <button type='submit'>Post</button>
                     <button type='button' onClick={onClose}>Cancel</button>
                 </form>
