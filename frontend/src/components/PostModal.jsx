088231a9acfa2085d2a9162dffdfb275a7c77eb7
Tobias Dodge <tobydodge65@gmail.com>
2024-06-25 12:47:13 -0230

    Super basic form until I can see what I am exactly doing

--- frontend/src/components/PostModal.jsx
+++ frontend/src/components/PostModal.jsx
@@ -1,2 +1,17 @@
 import { useState, useEffect } from 'react'
 import './Post'
+
+const PostModal = () => {
+
+
+    return (
+        <div className='post-modal'>
+            <form>
+                <textarea placeholder='Content' />
+                <button type='submit'>Post</button>
+            </form>
+        </div>
+    )
+}
+
+export default PostModal
