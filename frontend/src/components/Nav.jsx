253ea362860331b16e572f5636f792ffe213abb8
DRaymond <danimator1@gmail.com>
2024-06-28 09:37:56 -0400

    last update

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 import homeIcon from '../assets/images/Home.png'
 import addPostIcon from '../assets/images/AddPost.png'
-import Timeline from '../assets/images/Timeline.png'
 import profile from '../assets/images/profile.png'
 
 const Nav = ({ onOpenModal }) => {
@@ -17,12 +16,8 @@ const Nav = ({ onOpenModal }) => {
         <img src={addPostIcon} alt='Create Post' />
         
       </button>
-      <Link to="/timeline">
-        <div className="nav-item">
-          <img src={Timeline} alt='Timeline' />
       
-        </div>
-      </Link>
+      
       <Link to="/userprofile">
         <div className="nav-item">
         <img src={profile} alt='User Profile' />
